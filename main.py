842feef29f4a2cdfea986d6f08adabe32bd52e97
VeryCleverName3 <awesome827740@gmail.com>
2020-01-07 20:56:23 -0500

    Commented some of it :(

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import imutils
 
 cap = cv2.VideoCapture(0)
 
-value = 70
+value = 70 #For debugging
 
 portraitModeSixInchAreaHexagonThing = 2800
 
@@ -19,53 +19,59 @@ hsvValues = {
 }
 
 while 1:
-    _, img = cap.read()
+    _, img = cap.read() #Get frame of video
 
+    #For more debugging
     if keyboard.is_pressed("q"):
         value += 1
         print(value)
 
-    cap.set(cv2.CAP_PROP_EXPOSURE, -10)
+    cap.set(cv2.CAP_PROP_EXPOSURE, -10) #Set exposure lower
 
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV) #Convert image to hsv for color detection
 
+    #Lower and upper bounds of what is roughly green, will change later
     lower = np.array([hsvValues["baseHue"] - hsvValues["hueRange"], hsvValues["lowerSaturation"], hsvValues["lowerValue"]]) 
     upper = np.array([hsvValues["baseHue"] + hsvValues["hueRange"], hsvValues["upperSaturation"], hsvValues["upperValue"]]) 
 
-    mask = cv2.inRange(hsv, lower, upper)
+    mask = cv2.inRange(hsv, lower, upper) #Color mask
 
+    #Blur it to reduce noise, then expand it
     mask = cv2.medianBlur(mask, 15)
-
     mask = cv2.GaussianBlur(mask, (15, 15), 0)
 
+    #Get edges of image
     edges = cv2.Canny(img, 40, 100)
 
+    #Apply color mask to edges
     edges = cv2.bitwise_and(edges, edges, mask=mask)
 
+    #Get contours of image
     cnts = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
     cnts = imutils.grab_contours(cnts)
 
-    foundRectangle = False
+    foundTarget = False
 
     visionTargets = []
     areas = []
 
     for c in cnts:
-        M = cv2.moments(c)
+        M = cv2.moments(c) #Gets centers of contours
         if(M["m00"] != 0):
+            #Gets center coordinates of contour
             cX = int((M["m10"] / M["m00"]))
             cY = int((M["m01"] / M["m00"]))
-            peri = cv2.arcLength(c, True)
-            approx = cv2.approxPolyDP(c, 0.01 * peri, True)
+            peri = cv2.arcLength(c, True) #Perimeter of contour
+            approx = cv2.approxPolyDP(c, 0.01 * peri, True) #Poly approximation of contour
             #print(len(approx))
             #print(approx)
             if len(approx) == 8:
-                foundRectangle = True
+                foundTarget = True
+                #Append area and approx'ed points
                 visionTargets.append(approx)
                 areas.append(cv2.contourArea(c))
 
-    if foundRectangle:
+    if foundTarget:
         print(areas)
     else:
         print("No Vision. :(")
