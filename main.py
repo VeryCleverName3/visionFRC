e6e1fe903b98d7bb69df864d034d3904d02f407f
xHayden <93haydcarp@csdecatur.net>
2020-01-08 10:04:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,16 @@ from vision import Vision
 #Instantiate vision object to look for objects with 8 vertices
 vision = Vision(8)
 
+targets = vision.find()
+
 while 1:
-    print(vision.find())
+    targets = vision.find()
+    # vision.find returns an array of vision target objects
+    # targets[i].points are the vertices of the target
+    # targets[i].area is the area of the target in pixels
+def printTarget(target):
+    print(target.points)
+    
+printTarget(targets[0])
+
+    
